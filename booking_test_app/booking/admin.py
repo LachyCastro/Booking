64265abe299987f57d33c0772fc89c0f0bf52582
Lachy Castro <lachycastro9@gmail.com>
2024-08-29 10:07:28 +0200

    Update admin.py

--- booking_test_app/booking/admin.py
+++ booking_test_app/booking/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models.customer import Customer
+from .models.booking import Booking
+from .models.tour import Tour
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name','phone','email']
+    search_fields = ['id', 'first_name', 'last_name','phone','email']
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'date', 'state', 'customer', 'tour']
+    search_fields = ['id', 'date', 'state', 'customer', 'tour']
+
+class TourAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['id', 'name']
+
